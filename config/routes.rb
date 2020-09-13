46243c367f980b3eb1fbca9415f7b6dfd8d974d4
lcorbitt <movalley5@gmail.com>
2020-09-12 19:52:02 -0600

    Set up devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'home/index'
+
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
