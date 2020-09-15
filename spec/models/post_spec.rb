cec8ebb92e261e0ae6083721917d45513d32d1bb
lcorbitt <movalley5@gmail.com>
2020-09-14 19:57:49 -0600

    Update User and Post spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,22 +3,24 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   let(:post) { create :post }
 
-  it 'has a valid factory' do
-    expect(post).to be_valid
-  end
+  context 'validations' do
+    it 'has a valid factory' do
+      expect(post).to be_valid
+    end
 
-  it "is not valid without a user" do
-    post.user = nil
-    expect(post).to_not be_valid
-  end
+    it "is not valid without a user" do
+      post.user = nil
+      expect(post).to_not be_valid
+    end
 
-  it "is not valid without a title" do
-    post.title = nil
-    expect(post).to_not be_valid
-  end
+    it "is not valid without a title" do
+      post.title = nil
+      expect(post).to_not be_valid
+    end
 
-  it "is not valid without a body" do
-    post.body = nil
-    expect(post).to_not be_valid
+    it "is not valid without a body" do
+      post.body = nil
+      expect(post).to_not be_valid
+    end
   end
 end
