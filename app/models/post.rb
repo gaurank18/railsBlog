e6c040f66bdb9362cf81ecdd3f3155b293e11e77
lcorbitt <movalley5@gmail.com>
2020-09-14 19:34:12 -0600

    Add Post validations, Factory, and spec coverage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+
+  validates :user, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
 end
